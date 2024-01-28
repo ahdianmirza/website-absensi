9417efaf7faab68eacd4194811c90f9b68f5abe0
ahdianmirza <ahdianmirza@gmail.com>
2024-01-28 20:35:01 +0700

    Update tampilan navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,11 @@
 <nav class="w-full h-20 bg-secondary">
     <div class="mx-auto max-w-md h-full flex justify-between items-center">
-        <a href="/" class="text-xl text-primary font-semibold">Kehadiran</a>
-        <a href="/karyawan" class="text-xl text-primary font-semibold">Karyawan</a>
-        <a href="/data-kehadiran" class="text-xl text-primary font-semibold">Data Kehadiran</a>
+        <a href="/"
+            class="text-xl text-primary font-semibold hover:bg-slate-50 p-2 rounded-lg {{ Request::is('/') ? 'bg-slate-50' : '' }}">Kehadiran</a>
+        <a href="/karyawan"
+            class="text-xl text-primary font-semibold hover:bg-slate-50 p-2 rounded-lg {{ Request::is('karyawan*') ? 'bg-slate-50' : '' }}">Karyawan</a>
+        <a href="/data-kehadiran"
+            class="text-xl text-primary font-semibold hover:bg-slate-50 p-2 rounded-lg {{ Request::is('data-kehadiran') ? 'bg-slate-50' : '' }}">Data
+            Kehadiran</a>
     </div>
 </nav>
