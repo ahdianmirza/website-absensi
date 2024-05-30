841a40ac75d3e14f66a8b75301549822478cc7e7
ahdianmirza <ahdianmirza@gmail.com>
2024-05-30 20:55:21 +0700

    fixing fitur search berdasarkan name, uid dan jabatan

--- app/Models/DataKehadiran.php
+++ app/Models/DataKehadiran.php
@@ -11,9 +11,10 @@ class DataKehadiran extends Model
     protected $guarded = ['id'];
     public function scopeFilter($query, array $filters) {
         $query->when($filters['search'] ?? false, function ($query, $search) {
-            return $query->where('name', 'like', '%' . $search . '%');
-        })
-        ->when($filters['sort'] ?? false, function ($query, $sort) {
+            return $query->where('name', 'like', '%' . $search . '%')
+                ->orWhere('uid', 'like', '%' . $search . '%')
+                ->orWhere('jabatan', 'like', '%' . $search . '%');
+        })->when($filters['sort'] ?? false, function ($query, $sort) {
             return $query->orderBy('created_at', $sort);
         });
     }
--- resources/views/dataKehadiran/index.blade.php
+++ resources/views/dataKehadiran/index.blade.php
@@ -35,9 +35,20 @@
                     </form>
                 </div>
                 <div>
-                    <a href="{{ url('/data-kehadiran/export') }}"
-                        class="px-2 py-1.5 lg:px-4 lg:py-2 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                        Export
+                    <a href="{{ url('/data-kehadiran/export') }}">
+                        <div
+                            class="flex gap-x-2 items-center px-2 py-1.5 lg:px-4 lg:py-2 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="feather feather-download">
+                                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                    <polyline points="7 10 12 15 17 10"></polyline>
+                                    <line x1="12" y1="15" x2="12" y2="3"></line>
+                                </svg>
+                            </span>
+                            <p>Unduh</p>
+                        </div>
                     </a>
                 </div>
             </div>
