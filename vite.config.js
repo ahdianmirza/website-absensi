f8dcc1b5110ba1314aadebbe1509903a511a5643
ahdianmirza <ahdianmirza@gmail.com>
2024-05-30 22:01:56 +0700

    fixing bug vite

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,47 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path'
+import "./resources/css/app.css";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'node_modules/flowbite/dist/flowbite.js',
-                'node_modules/flowbite/dist/flowbite.css'
+                path.resolve(__dirname, "resources/css/app.css"),
+                path.resolve(__dirname, "resources/js/app.js"),
+                path.resolve(
+                    __dirname,
+                    "node_modules/flowbite/dist/flowbite.js"
+                ),
+                path.resolve(
+                    __dirname,
+                    "node_modules/flowbite/dist/flowbite.css"
+                ),
             ],
             refresh: true,
         }),
+        commonjs()
     ],
     build: {
+        manifest: true,
         rollupOptions: {
             input: {
-                main: path.resolve(__dirname, 'resources/js/app.js'),
-                flowbite_js: path.resolve(__dirname, 'node_modules/flowbite/dist/flowbite.js'),
-                flowbite_css: path.resolve(__dirname, 'node_modules/flowbite/dist/flowbite.css')
+                main_css: path.resolve(__dirname, "resources/css/app.css"),
+                main_js: path.resolve(__dirname, "resources/js/app.js"),
+                flowbite_js: path.resolve(
+                    __dirname,
+                    "node_modules/flowbite/dist/flowbite.js"
+                ),
+                flowbite_css: path.resolve(
+                    __dirname,
+                    "node_modules/flowbite/dist/flowbite.css"
+                ),
             },
             output: {
                 // Set output directory
-                dir: 'public/build'
-            }
-        }
-    }
+                dir: "public/build",
+            },
+        },
+    },
 });
